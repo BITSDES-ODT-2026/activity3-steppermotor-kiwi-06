38d9de44219940c7217386d9059b2fd3c74bc014
Devashri Patel <devashripatel2006@gmail.com>
2026-02-17 15:10:43 +0530

    Update StepperMotor.py

--- StepperMotor.py
+++ StepperMotor.py
@@ -1,1 +1,32 @@
+from machine import Pin
+import time
 
+#first i = [1,0,0,0] , i[0]
+in1 = Pin(5,Pin.OUT)
+in2 = Pin(14,Pin.OUT)
+in3 = Pin(15,Pin.OUT)
+in4 = Pin(19,Pin.OUT)
+
+
+step = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+step2= [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]
+
+
+
+while True:
+    for k in range(500):
+        for i in step:
+            in1.value(i[0])
+            in2.value(i[1])
+            in3.value(i[2])
+            in4.value(i[3])
+            time.sleep_ms(5)
+            
+    for p in range(500):
+        for o in step2:
+            in1.value(i[0])
+            in2.value(i[1])
+            in3.value(i[2])
+            in4.value(i[3])
+            time.sleep_ms(5)
+      
